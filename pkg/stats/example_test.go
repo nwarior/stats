304d94d421a5c919161c2d4d19d5d0201845c687
Nurbek <samoindig@gmail.com>
2021-11-28 10:41:49 +0500

    добавлен пакет stats

---
+++ pkg/stats/example_test.go
@@ -0,0 +1,52 @@
+﻿package stats
+
+import (
+	"github.com/nwarior/bank/pkg/types"
+	"fmt"
+)
+
+func ExampleAvg(){
+	payments := []types.Payment {
+		{
+			ID: 101,
+			Amount: 10_00,
+			Category: "ресторан",
+		},
+		{
+			ID: 103,
+			Amount: 20_00,
+			Category: "Burger",
+		},
+		{
+			ID: 101,
+			Amount: -10_00,
+			Category: "nowhere",
+		},
+	}
+
+	fmt.Println(Avg(payments))
+	// Output: 1500
+}
+
+func ExampleTotalInCategory(){
+	payments := []types.Payment {
+		{
+			ID: 101,
+			Amount: 10_00,
+			Category: "ресторан",
+		},
+		{
+			ID: 103,
+			Amount: 20_00,
+			Category: "ресторан",
+		},
+		{
+			ID: 103,
+			Amount: -10_00,
+			Category: "nowhere",
+		},
+	}
+
+	fmt.Println(TotalInCategory(payments, "ресторан"))
+	// Output: 3000
+}
---
+++ pkg/stats/stats.go
@@ -0,0 +1,37 @@
+﻿package stats
+
+import (
+	"github.com/nwarior/bank/pkg/types"
+)
+
+// Avg рассчитывает среднюю сумму платежа.
+func Avg(payments []types.Payment) types.Money {
+	var sum types.Money
+	sum = 0
+
+	var index int
+	index = 0
+
+	for _, payment := range payments {
+		if payment.Amount > 0 {
+			sum += types.Money(payment.Amount)
+			index = index + 1
+		}
+	}
+	return sum / types.Money(index)
+}
+
+// TotalInCategory находит сумму покупок в определённой категории.
+func TotalInCategory(payments []types.Payment, category types.Category) types.Money {
+
+	var sum types.Money
+	sum = 0
+	
+	for _, payment := range payments {
+		if payment.Category == category && payment.Amount > 0 {
+			sum += payment.Amount
+		}
+	}
+
+	return sum
+}
