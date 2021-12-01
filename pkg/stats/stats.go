b4fae6019e8d17f78b9f1daf21808faafde1d887
Nurbek <samoindig@gmail.com>
2021-12-01 05:41:40 +0500

    добавлена CategoriesTotal

--- pkg/stats/stats.go
+++ pkg/stats/stats.go
@@ -35,3 +35,14 @@ func TotalInCategory(payments []types.Payment, category types.Category) types.Mo
 
 	return sum
 }
+
+// CategoriesTotal возвращает сумму платежей по каждой категории.
+func CategoriesTotal(payments []types.Payment) map[types.Category]types.Money {
+	categories := map[types.Category]types.Money{}
+
+	for _, payment := range payments {
+		categories[payment.Category] += payment.Amount
+	}
+
+	return categories
+}
