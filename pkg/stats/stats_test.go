7b109e52d18f1c73c603c6f5777c23e2a9191ac2
Nurbek <samoindig@gmail.com>
2021-12-02 15:55:21 +0500

    добавлен PeriodsDynamic

--- pkg/stats/stats_test.go
+++ pkg/stats/stats_test.go
@@ -126,3 +126,29 @@ func TestCategoriesAvg(t *testing.T) {
 		t.Errorf("invalid result, expected: %v, actual: %v", expected, result)
 	}
 }
+
+func TestPeriodsDynamic(t *testing.T) {
+	first := map[types.Category]types.Money{
+		"auto": 10,
+		"food": 20,
+	}
+
+	second := map[types.Category]types.Money {
+		"auto": 10,
+		"food": 25,
+		"mobile": 5,
+	}
+
+	expected := map[types.Category]types.Money {
+		"auto": 0,
+		"food": 5,
+		"mobile": 5,
+	}
+
+	result := PeriodsDynamic(first, second)
+
+	if !reflect.DeepEqual(expected, result) {
+		t.Errorf("invalid result, expected: %v, actual: %v", expected, result)
+	}
+}
+
